3852586c204e1fbb8bff222a8b4e70d01a2e3693
Víctor Martínez <vito.marodriguez@gmail.com>
2025-06-06 11:41:18 -0600

    Codigo listo para produccion

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
 import { defineConfig } from 'vite'
-import * as path from 'path'
+import path from 'path'
 
 
 export default defineConfig({
+  server: {
+    port: 3000,
+    open: true, // Abre el navegador al iniciar
+  },
+
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: { 
+        drop_console: true,
+        pure_funcs: ['console.info'],
+      }, // Ejemplo: eliminar console.log
+    },
+  },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@components':path.resolve(__dirname, './src/components'),
-      '@interfaces':path.resolve(__dirname, './src/interfaces'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@interfaces': path.resolve(__dirname, './src/interfaces'),
     },
   },
 })
